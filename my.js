6f2bc81890f5952701e41096bc9e482e33f5e3d0
shehan82 <shehansandeepa82@gmail.com>
2020-07-16 13:32:06 +0530

    Start learning JS

---
+++ my.js
@@ -0,0 +1,12 @@
+console.log("hello");
+var b = "my name is shehan";
+console.log(b);
+
+
+//var name = prompt("what is your name man");
+//document.getElementById("first").innerHTML= "Your name is : " + name;
+
+// numbers in java script
+
+var num = 5.2;
+console.log(2*num);
