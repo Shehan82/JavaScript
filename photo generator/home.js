c1b62a721a9c911de090d6bcdf6a6830983a4c0b
shehan82 <shehansandeepa82@gmail.com>
2020-07-17 00:07:53 +0530

    simple photo generator

---
+++ photo generator/home.js
@@ -0,0 +1,6 @@
+function gen()
+{
+    var image = document.createElement("img");
+    image.src="https://i.picsum.photos/id/866/200/300.jpg?hmac=rcadCENKh4rD6MAp6V_ma-AyWv641M4iiOpe1RyFHeI";
+    document.getElementById("flex").appendChild(image);
+}
